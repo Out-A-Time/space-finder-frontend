5a55eb51f767bad0f7cfb4ac53738c3024169279
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-02-24 15:16:01 +0000

    test component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-  return <div className="App">App Works</div>;
+  return (
+    <div className="App">
+      App Works
+      <div className="parent">
+        This is the parent component
+        <div className="child">This is the child component</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
